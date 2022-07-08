b94f04a0f189ff16311ec014b7e5b0a430e5872a
eldiiar Almazbek <eldiiaralmazbekov@gmail.com>
2022-07-09 01:10:16 +0600

    completed user repository

--- packages/user_repository/lib/src/user_repository.dart
+++ packages/user_repository/lib/src/user_repository.dart
@@ -1,7 +1,30 @@
-/// {@template user_repository}
-/// A package which manages the user domain.
-/// {@endtemplate}
+// ignore_for_file: public_member_api_docs
+
+import 'package:supabase_auth_client/supabase_auth_client.dart';
+import 'package:supabase_database_client/supabase_database_client.dart';
+
 class UserRepository {
-  /// {@macro user_repository}
-  const UserRepository();
+  const UserRepository(
+    SupabaseAuthClient authClient,
+    SupabaseDatabaseClient databaseClient,
+  )   : _authClient = authClient,
+        _databaseClient = databaseClient;
+
+  final SupabaseAuthClient _authClient;
+  final SupabaseDatabaseClient _databaseClient;
+
+  Future<SupabaseUser> getUser() async {
+    final supabaseUser = await _databaseClient.getUserProfile();
+    return supabaseUser;
+  }
+
+  Future<void> updateUser(SupabaseUser user) async {
+    await _databaseClient.updateUser(user);
+  }
+
+  Future<void> singIn({required String email, required bool isWeb}) async {
+    return _authClient.signIn(email: email, isWeb: isWeb);
+  }
+
+  Future<void> signOut() async => _authClient.signOut();
 }
--- packages/user_repository/test/src/user_repository_test.dart
+++ packages/user_repository/test/src/user_repository_test.dart
@@ -1,11 +1,22 @@
 // ignore_for_file: prefer_const_constructors
 import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:supabase_auth_client/supabase_auth_client.dart';
+import 'package:supabase_database_client/supabase_database_client.dart';
 import 'package:user_repository/user_repository.dart';
+
+class MockSupabaseAuthClient extends Mock implements SupabaseAuthClient {}
+
+// ignore: lines_longer_than_80_chars
+class MockSupabaseDatabaseClient extends Mock implements SupabaseDatabaseClient {}
 
 void main() {
   group('UserRepository', () {
     test('can be instantiated', () {
-      expect(UserRepository(), isNotNull);
+      expect(
+        UserRepository(MockSupabaseAuthClient(), MockSupabaseDatabaseClient()),
+        isNotNull,
+      );
     });
   });
 }
