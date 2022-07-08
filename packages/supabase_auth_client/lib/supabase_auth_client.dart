862d3df0de9e359075d766487ab6904d7b5f391d
eldiiar Almazbek <eldiiaralmazbekov@gmail.com>
2022-07-09 00:57:15 +0600

    added some exports

--- packages/supabase_auth_client/lib/supabase_auth_client.dart
+++ packages/supabase_auth_client/lib/supabase_auth_client.dart
@@ -1,3 +1,4 @@
 library supabase_auth_client;
 
+export 'src/exceptions.dart';
 export 'src/supabase_auth_client.dart';
--- packages/supabase_database_client/lib/supabase_database_client.dart
+++ packages/supabase_database_client/lib/supabase_database_client.dart
@@ -1,3 +1,5 @@
 library supabase_database_client;
 
+export 'src/exception/exception.dart';
+export 'src/models/models.dart';
 export 'src/supabase_database_client.dart';
