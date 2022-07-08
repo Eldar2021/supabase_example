6c32730510acec34453d07361599216f5e3697b4
eldiiar Almazbek <eldiiaralmazbekov@gmail.com>
2022-07-09 00:55:11 +0600

    added user repository to packages

---
+++ packages/user_repository/lib/src/user_repository.dart
@@ -0,0 +1,7 @@
+/// {@template user_repository}
+/// A package which manages the user domain.
+/// {@endtemplate}
+class UserRepository {
+  /// {@macro user_repository}
+  const UserRepository();
+}
