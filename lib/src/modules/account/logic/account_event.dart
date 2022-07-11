17d847ddeaee2f730aec5dd172f46b2d4f69412f
eldiiar Almazbek <eldiiaralmazbekov@gmail.com>
2022-07-11 10:30:43 +0600

    completed account event

--- lib/src/modules/account/logic/account_event.dart
+++ lib/src/modules/account/logic/account_event.dart
@@ -6,3 +6,30 @@ abstract class AccountEvent extends Equatable {
   @override
   List<Object> get props => [];
 }
+
+class AccountInfoFetchEvent extends AccountEvent {}
+
+class AccountUserUpdateEvent extends AccountEvent {
+  const AccountUserUpdateEvent(this.user);
+
+  final SupabaseUser user;
+  @override
+  List<Object> get props => [user];
+}
+
+class AccountUserNameChangeEvent extends AccountEvent {
+  const AccountUserNameChangeEvent(this.userName);
+
+  final String userName;
+  @override
+  List<Object> get props => [userName];
+}
+
+class AccountCompanyNameChangeEvent extends AccountEvent {
+  const AccountCompanyNameChangeEvent(this.companyName);
+
+  final String companyName;
+
+  @override
+  List<Object> get props => [companyName];
+}
