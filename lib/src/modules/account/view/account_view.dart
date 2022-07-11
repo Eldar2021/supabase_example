ab7c70dbcbe912af8e4045f2821b1ad5d3a0307d
eldiiar Almazbek <eldiiaralmazbekov@gmail.com>
2022-07-11 12:58:03 +0600

    completed account view

--- lib/src/modules/account/view/account_view.dart
+++ lib/src/modules/account/view/account_view.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
+import '../../../src.dart';
+import '../widgets/widgets.dart';
+
 class AccountView extends StatelessWidget {
   const AccountView({super.key});
 
@@ -8,10 +11,28 @@ class AccountView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Account')),
-      // body: BlocConsumer(
-      //   listener: (context, state) {},
-      //   builder: (context, state) {},
-      // ),
+      body: BlocConsumer<AccountBloc, AccountState>(
+        listener: (context, state) {
+          if (state.status.isUpdate) context.showNackBar(message: 'Updated!');
+        },
+        buildWhen: (previous, current) =>
+            // ignore: lines_longer_than_80_chars
+            current.status.isSuccess || current.status.isUpdate || current.status.isEditing,
+        builder: (context, state) {
+          return ListView(
+            padding: const EdgeInsets.all(28),
+            children: const [
+              AccountHeader(),
+              UserNameTextField(),
+              CompanyNameTextField(),
+              SizedBox(height: 50),
+              UpdateUserButton(),
+              SizedBox(height: 18),
+              SignOutButton(),
+            ],
+          );
+        },
+      ),
     );
   }
 }
